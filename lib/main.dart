0e8ffc004b9a1a724510d80088b3cd26b9368a9f
sarangvs2000 <sarang.vs@yahoo.com>
2025-06-26 16:30:09 +0530

    set up for firebase in ios

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:chat_app/app.dart';
 import 'package:chat_app/data/data_sources/local/user_local_datasource.dart';
 import 'package:chat_app/presentation/auth/bloc/auth_bloc.dart';
 import 'package:chat_app/presentation/splash/bloc/splash_cubit.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await configureDependencies();
   await getIt<UserLocalDatasource>().init();
 
