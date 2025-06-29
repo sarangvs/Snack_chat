86db5f10665b37d641912c8938bb931a81735a8e
sarangvs2000 <sarang.vs@yahoo.com>
2025-06-29 16:33:11 +0530

    app update

--- lib/DI/dependency_injection.dart
+++ lib/DI/dependency_injection.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
-import 'dependency_injection.config.dart'; // Generated file
+import 'dependency_injection.config.dart';
 
 final getIt = GetIt.instance;
 
